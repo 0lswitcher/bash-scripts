0cdcd1941f52e625a722ba16f365290b7a80a94b
0lswitcher <manjarodistro@gmail.com>
2025-09-03 02:24:28 -0500

    Update swww-as-theme.sh

--- scripts/swww-as-theme.sh
+++ scripts/swww-as-theme.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# swww-as-theme.sh - convert current background / wallpaper to pywal theme
 
 if [ -f "$HOME/.cache/current-swww-img" ]; then
     theme=$(<"$HOME/.cache/current-swww-img")
